376cb88fdc8e4193d20049a1516b424f0ffc4b41
Gary Tate <me@garytate.co.uk>
2020-01-15 17:30:33 +0000

    Added functionality to retrieve all containers.

--- src/Program.cs
+++ src/Program.cs
@@ -3,115 +3,120 @@ using System.Collections.Generic;
 
 namespace Hazel
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			// Console.WriteLine();
-			// string input = Console.ReadLine();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine();
+            // string input = Console.ReadLine();
 
-			// Class imports
-			Display displayObj = new Display();
-			FileManager fileManagerObj = new FileManager();
-			Character characterObj = new Character();
-			Activity activityObj = new Activity();
-			Keywords keywordObj = new Keywords();
+            // Class imports
+            Display displayObj = new Display();
+            FileManager fileManagerObj = new FileManager();
+            Character characterObj = new Character();
+            Activity activityObj = new Activity();
+            Keywords keywordObj = new Keywords();
+            Container ContainerObj = new Container();
+            
+            // Initialising the program
+            string appTitle = "Hazel";
+            string appVersion = "1.0.0";
+            string appAuthor = "Gary Tate";
+            string appDirectory = System.Environment.CurrentDirectory;
+            bool active = true;
 
-			// Initialising the program
-			string appTitle = "Hazel";
-			string appVersion = "1.0.0";
-			string appAuthor = "Gary Tate";
-			string appDirectory = System.Environment.CurrentDirectory;
-			bool active = true;
+            string[] options = {
+                "Reload Files",
+                "Select Timerange [WIP]", 
+                "Check Activity",
+                "Search for Keywords",
+                "Get Log Files",
+                "Get Characters",
+                "Get Containers",
+                "Quit Program"
+            };
 
-			string[] options = {
-				"Reload Files",
-				"Select Timerange [WIP]",
-				"Check Activity",
-				"Search for Keywords",
-				"Get Log Files",
-				"Get Characters",
-				"Quit Program"
-			};
+            // Welcome Message
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("{0}: Version {1} maintained by {2}.\n", appTitle, appVersion, appAuthor);
+            Console.ResetColor();
 
-			// Welcome Message
-			Console.ForegroundColor = ConsoleColor.Cyan;
-			Console.WriteLine("{0}: Version {1} maintained by {2}.\n", appTitle, appVersion, appAuthor);
-			Console.ResetColor();
+            List<string> logFiles = fileManagerObj.getAllLogs(appDirectory);
+            List<string> characters = characterObj.getAllCharacters(appDirectory);
 
-			List<string> logFiles = fileManagerObj.getAllLogs(appDirectory);
-			List<string> characters = characterObj.getAllCharacters(appDirectory);
+            while (active) {
+                displayObj.DisplayMenu(options, logFiles, characters);
+                Console.Write("> ");
+                string keyInput = Console.ReadKey().Key.ToString();
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("{0}: Version {1} maintained by {2}.\n", appTitle, appVersion, appAuthor);
+                Console.ResetColor();
+                switch (keyInput)
+                {
+                    case "D1":
+                        //Reload Characters
+                        logFiles = fileManagerObj.getAllLogs(appDirectory);
+                        Console.WriteLine("Files reloaded.");
+                        break;
+                    case "D2":
+                        // Select log timerange
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("{0} not yet supported.", options[1]);
+                        Console.ResetColor();
+                        break;
+                    case "D3":
+                        // Check Activity
+                        activityObj.displayActivity(appDirectory, characters, logFiles);
+                        break;
+                    case "D4":
+                        // Check for Keywords
+                        List<string> keywords = keywordObj.getKeywords();
+                        keywordObj.displayKeywords(appDirectory, characters, logFiles, keywords);
+                        break;
+                    case "D5":
+                        // Get Log Files
+                        foreach (string logFile in logFiles)
+                        {
+                            Console.WriteLine(logFile);
+                        }
+                        break;
+                    case "D6":
+                        // Get Characters
+                        foreach (string character in characters)
+                        {
+                            Console.WriteLine(character);
+                        }
+                        break;
+                    case "D7":
+                        // containers
+                        ContainerObj.returnAllContainers(logFiles);
+                        break;
+                    case "D8":
+                        // Exit program
+                        System.Environment.Exit(1);
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Incorrect option.");
+                        Console.ResetColor();
+                        break;
+                }
+                Console.WriteLine();
+                
+            }
 
-			while (active)
-			{
-				displayObj.DisplayMenu(options, logFiles, characters);
-				Console.Write("> ");
-				string keyInput = Console.ReadKey().Key.ToString();
-				Console.Clear();
-				Console.ForegroundColor = ConsoleColor.Cyan;
-				Console.WriteLine("{0}: Version {1} maintained by {2}.\n", appTitle, appVersion, appAuthor);
-				Console.ResetColor();
-				switch (keyInput)
-				{
-					case "D1":
-						//Reload Characters
-						logFiles = fileManagerObj.getAllLogs(appDirectory);
-						Console.WriteLine("Files reloaded.");
-						break;
-					case "D2":
-						// Select log timerange
-						Console.ForegroundColor = ConsoleColor.Red;
-						Console.WriteLine("{0} not yet supported.", options[1]);
-						Console.ResetColor();
-						break;
-					case "D3":
-						// Check Activity
-						activityObj.displayActivity(appDirectory, characters, logFiles);
-						break;
-					case "D4":
-						// Check for Keywords
-						List<string> keywords = keywordObj.getKeywords();
-						keywordObj.displayKeywords(appDirectory, characters, logFiles, keywords);
-						break;
-					case "D5":
-						// Get Log Files
-						foreach (string logFile in logFiles)
-						{
-							Console.WriteLine(logFile);
-						}
-						break;
-					case "D6":
-						// Get Characters
-						foreach (string character in characters)
-						{
-							Console.WriteLine(character);
-						}
-						break;
-					case "D7":
-						// Exit program
-						System.Environment.Exit(1);
-						break;
-					default:
-						Console.ForegroundColor = ConsoleColor.Red;
-						Console.WriteLine("Incorrect option.");
-						Console.ResetColor();
-						break;
-				}
-				Console.WriteLine();
-
-			}
-
-			/*foreach(string logFile in logFiles)
+            /*foreach(string logFile in logFiles)
             {
                 Console.WriteLine(logFile);
             }*/
 
-			/*foreach (string logs in fileManagerObj.getAllLogs())
+            /*foreach (string logs in fileManagerObj.getAllLogs())
             {
                 Console.WriteLine(logs);
             }*/
-		}
+        }
 
-	}
+    }
 
 }
